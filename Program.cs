73790bfe577efb50ae25bd5e809e144aa21dd5aa
CanTalat-Yakan <57494543+CanTalat-Yakan@users.noreply.github.com>
2024-10-23 11:00:42 +0200

    ⠀

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@ internal class Program
 {
     [STAThread]
     private static void Main() =>
-        new Engine.Program().Run(initialization: () =>
-            Engine.Kernel.Instance.SystemManager.MainEntityManager.CreateEntity().AddComponent<GameManager>());
+        new Engine.Program().Run(
+            config: Engine.Config.GetDefault(
+                multiSample: Engine.MultiSample.None,
+                title: "Voxel-Sandbox",
+                width: 2560, height: 1440),
+            initialization: () =>
+                Engine.Kernel.Instance.SystemManager.MainEntityManager.CreateEntity().AddComponent<GameManager>());
 }
